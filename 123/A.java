1b46ae005a438202ebfe88d0a0965852668d8e6d
Tanu <stanu2966@gmail.com>
2024-06-30 13:25:43 +0530

    first commit

---
+++ 123/A.java
@@ -0,0 +1,20 @@
+class A
+{
+    int a=10;
+    static int b=20;
+    void add ()
+    {
+        int c=30, d;
+        d=a+b+c;
+        System.out.println(d);
+        
+    }
+    void multiply()
+    {
+        int e=40, f;
+        f=a*b*e;
+        System.out.println(f);
+    }
+
+    
+    }
---
+++ 123/aruns.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+ public class aruns
+{
+    public static void kutta()
+    {
+       Scanner sc = new Scanner(System.in); 
+        int a,b,c;
+        System.out.println(" what is a ");
+        a =  sc.nextInt();
+        System.out.println(" what is b ");
+        b = sc.nextInt();
+        c = a*b;
+
+    System.out.println(c);
+    }
+}
+
